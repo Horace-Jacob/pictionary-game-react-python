5f88040d3adc5cc2860efde12d904f0339af0e54
Lwin Naing <l.naing@intelance.co.uk>
2024-02-14 00:13:27 +0630

    update readme.md and add env example files

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "<your_secret_key>"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "<secret_key>"
     PORT = os.getenv('PORT', '5000')
     SQLALCHEMY_DATABASE_URI = os.environ.get("PSQL_URL") or 'postgresql://<username>:<password>@<host>/<db>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -10,10 +10,10 @@ class Config:
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_KEY_PREFIX = 'your_prefix'
-    SESSION_REDIS = '<your_redis_host>'
+    SESSION_REDIS = '<YOUR_REDIS_HOST>'
 
-    GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID") or ""
-    GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET") or ""
+    GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID") or "<YOUR_GOOGLE_CLIENT_ID>"
+    GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET") or "<YOUR_GOOGLE_CLIENT_SECRET>"
 
     OAUTHLIB_INSECURE_TRANSPORT = True
     
