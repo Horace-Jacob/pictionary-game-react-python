9c9091c6ccbeb6d8ad561714e0f5d777e9968a25
Lwin Naing <l.naing@intelance.co.uk>
2024-02-14 00:05:01 +0630

    change readme.md file

--- server/config.py
+++ server/config.py
@@ -1,16 +1,16 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "pictionarygamekey"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "<your_secret_key>"
     PORT = os.getenv('PORT', '5000')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("PSQL_URL") or 'postgresql://postgres:awesomepassword123@localhost:5432/pictionary_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("PSQL_URL") or 'postgresql://<username>:<password>@<host>/<db>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # redis connection
     SESSION_TYPE = 'redis'
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
     SESSION_KEY_PREFIX = 'your_prefix'
-    SESSION_REDIS = 'redis://localhost:6379'
+    SESSION_REDIS = '<your_redis_host>'
 
     GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID") or ""
     GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET") or ""
